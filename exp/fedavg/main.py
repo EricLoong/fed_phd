5607793dac19b2ad7f82aa1a282f059b09f10c97
EricLoong <ericlong0817@gmail.com>
2024-04-27 21:22:28 +0100

    Update main.py

--- exp/fedavg/main.py
+++ exp/fedavg/main.py
@@ -200,7 +200,6 @@ if __name__ == "__main__":
     np.random.seed(args.seed)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed_all(args.seed)
-    torch.backends.cudnn.deterministic = True
 
     # load data and generated image prepared for FID calculation
     print('Dataset dir is :', args.data_dir)
