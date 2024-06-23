043594fc1f29f186e0698ae6ded94d321fc828c4
EricLoong <ericlong0817@gmail.com>
2024-06-23 01:01:27 +0100

    Create cifar10_iid_test.sh

---
+++ exp/fedavg/cifar10_iid_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python main.py --dataset 'cifar10' \
+--partition_method 'iid' \
+--batch_size 128 \
+--lr 2e-4 \
+--epochs 1 \
+--client_num_in_total 20 --frac 0.2 \
+--sample_every 500 \
+--comm_round 20000 \
+--seed 2023 \
+--save \
+--calculate_fid \
+--tqdm
