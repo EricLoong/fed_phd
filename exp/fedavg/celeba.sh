4cf51db7b5f0c6d95317ab362e85c88fe8d4bbe0
EricLoong <ericlong0817@gmail.com>
2024-08-13 02:10:11 +0100

    update

--- exp/fedavg/celeba.sh
+++ exp/fedavg/celeba.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 python main.py --dataset 'celeba' \
 --partition_method 'noniid-pathological' \
---batch_size 64 \
+--batch_size 32 \
 --lr 2e-4 \
 --epochs 5 \
 --client_num_in_total 20 --frac 0.2 \
---sample_every 500 \
---comm_round 5000 \
+--sample_every 100 \
+--comm_round 1000 \
 --seed 2023 \
 --save \
 --calculate_fid \
