b4f28eb7c892b439d4e5c2f3e4a96988d8692a77
EricLoong <ericlong0817@gmail.com>
2024-08-16 04:04:30 +0100

    Update cifar10.sh

--- exp/fedphd/cifar10.sh
+++ exp/fedphd/cifar10.sh
@@ -14,7 +14,7 @@ python main.py --dataset 'cifar10' \
 --aggr_freq 5 \
 --st_rounds 200 \
 --pruning_ratio 0.2 \
---balance_agg_a 20000 \
+--balance_agg_a 50000 \
 --num_edge_servers 2 \
 --sparse_training \
 --tqdm
