cb73f6e3a5e21d80286b672b97f4ac2590bc3fc4
EricLoong <ericlong0817@gmail.com>
2024-08-16 01:54:05 +0100

    Update celeba.sh

--- exp/fedavg/celeba.sh
+++ exp/fedavg/celeba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 python main.py --dataset 'celeba' \
 --partition_method 'noniid-pathological' \
---batch_size 32 \
+--batch_size 64 \
 --lr 2e-4 \
 --epochs 5 \
 --client_num_in_total 20 --frac 0.2 \
