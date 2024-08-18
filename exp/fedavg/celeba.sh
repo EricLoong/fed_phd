3d0e4b89a2182b0ea9a71e4a5528a57104b4889f
EricLoong <ericlong0817@gmail.com>
2024-08-18 15:40:08 +0100

    Update celeba.sh

--- exp/fedavg/celeba.sh
+++ exp/fedavg/celeba.sh
@@ -5,7 +5,7 @@ python main.py --dataset 'celeba' \
 --lr 2e-4 \
 --epochs 5 \
 --client_num_in_total 20 --frac 0.2 \
---sample_every 100 \
+--fid_freq 100 \
 --comm_round 1000 \
 --seed 2023 \
 --save \
