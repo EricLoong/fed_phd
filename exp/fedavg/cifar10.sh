350c051527938cde840dafcbd0feb4e99444b512
EricLoong <ericlong0817@gmail.com>
2024-08-29 17:00:33 +0800

    Update cifar10.sh

--- exp/fedavg/cifar10.sh
+++ exp/fedavg/cifar10.sh
@@ -3,10 +3,10 @@ python main.py --dataset 'cifar10' \
 --partition_method 'noniid-pathological' \
 --batch_size 128 \
 --lr 2e-4 \
---epochs 1 \
+--epochs 5 \
 --client_num_in_total 50 --frac 0.2 \
 --sample_every 500 \
---comm_round 10000 \
+--comm_round 2000 \
 --seed 2023 \
 --save \
 --calculate_fid \
