bb70fe262ef613d864e77a7479eec47210d822a5
EricLoong <ericlong0817@gmail.com>
2024-10-27 00:06:44 +0800

    update

--- exp/fedavg/cifar10_iid.sh
+++ exp/fedavg/cifar10_iid.sh
@@ -3,10 +3,10 @@ python main.py --dataset 'cifar10' \
 --partition_method 'iid' \
 --batch_size 128 \
 --lr 2e-4 \
---epochs 5 \
---client_num_in_total 20 --frac 0.2 \
+--epochs 10 \
+--client_num_in_total 10 --frac 1 \
 --sample_every 500 \
---comm_round 10000 \
+--comm_round 1000 \
 --seed 2023 \
 --save \
 --calculate_fid \
--- exp/fedphd/cifar10.sh
+++ exp/fedphd/cifar10.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 python main.py --dataset 'cifar10' \
---partition_method 'iid' \
+--partition_method 'noniid-pathological' \
 --batch_size 128 \
 --lr 2e-4 \
 --epochs 1 \
---client_num_in_total 20 --frac 0.2 \
+--client_num_in_total 100 --frac 0.2 \
 --sample_every 500 \
 --comm_round 10000 \
 --seed 2023 \
