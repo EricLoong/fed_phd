e40d7b77a5c8dda296292610a70cb36c86fd6662
EricLoong <ericlong0817@gmail.com>
2024-11-17 21:02:31 +0800

    Update cifar10.sh

--- exp/moon/cifar10.sh
+++ exp/moon/cifar10.sh
@@ -5,7 +5,7 @@ python main.py --dataset 'cifar10' \
 --lr 2e-4 \
 --epochs 5 \
 --client_num_in_total 20 --frac 0.2 \
---contrastive_loss_weight 0.01 \
+--contrastive_loss_weight 1 \
 --sample_every 500 \
 --comm_round 10000 \
 --seed 2023 \
